d2793978cb83f18ef865cce986f268c5609e10f4
Сергей <smolanovsergey@gmail.com>
2024-11-04 17:15:58 +0200

    templates

--- wp-content/themes/fire-truck-center/blog.php
+++ wp-content/themes/fire-truck-center/blog.php
@@ -1,16 +1,12 @@
 <?php
 /**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Fire_Truck_Center
+Template Name: Blog
  */
 $post_id = get_the_ID();
 get_header();
 ?>
 
-	<main class="main">
+    <main class="main">
         <section class="banner-image">
             <div class="banner-image__image">
                 <img src="<?php echo the_field('image_banner', 1358) ?>" alt="BLOG">
@@ -29,35 +25,47 @@ get_header();
             </div>
         </section>
         <ul class="post__list main-container">
-            <?php if ( have_posts() ) : ?>
-                <?php
-                while ( have_posts() ) :
-                    the_post();
-                        ?>
-                            <li class="post__list-item">
-                                <a href="<?php the_permalink();?>" class="post__list-img" title="<?php the_title();?>">
-                                    <?php the_post_thumbnail('medium');?>
-                                </a>
-                                <a href="<?php the_permalink();?>" class="post__list-lnk" title="<?php the_title();?>">
-                                    <h2 class="post__list-title">
-                                        <?php the_title();?>
-                                    </h2>
-                                </a>
-                                <span class="post__list-except">
-                                    <?php the_excerpt();?>
+            <?php
+
+            $wp_query = new WP_Query(array(
+                "post_type" => "post",
+                "post_status" => "publish",
+                "posts_per_page" => -1
+            ));
+
+            if($wp_query->have_posts()) {
+
+                while($wp_query->have_posts()) {
+
+                    $wp_query->the_post();
+                    $post_id_in = get_the_ID();
+                    ?>
+
+                    <li class="post__list-item">
+                        <a href="<?php the_permalink();?>" class="post__list-img" title="<?php the_title();?>">
+                            <?php the_post_thumbnail('medium');?>
+                        </a>
+                        <a href="<?php the_permalink();?>" class="post__list-lnk" title="<?php the_title();?>">
+                            <h2 class="post__list-title">
+                                <?php the_title();?>
+                            </h2>
+                        </a>
+                        <span class="post__list-except">
+                            <?php echo the_field('exerpt', $post_id_in) ?>
+<!--                                    --><?php //the_excerpt();?>
                                 </span>
-                                <a href="<?php the_permalink();?>" class="post__list-more" title="<?php the_title();?>">
-                                    <?php echo 'Read more'; ?>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/header/more.svg" alt="<?php the_title();?>">
-                                </a>
-                            </li>
-                        <?php
-                endwhile;
+                        <a href="<?php the_permalink();?>" class="post__list-more" title="<?php the_title();?>">
+                            <?php echo 'Read more'; ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/header/more.svg" alt="<?php the_title();?>">
+                        </a>
+                    </li>
+
+                    <?php
+
+                }
                 echo "<div class='pagination'>";
                 global $wp_query;
-
                 $big = 999999999; // уникальное число для замены
-
                 $args = array(
                     'base'               => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
                     'format'             => '?paged=%#%',
@@ -71,14 +79,13 @@ get_header();
                     'before_page_number' => '<span class="page-num">',
                     'after_page_number'  => '</span>',
                 );
-
                 echo paginate_links($args);
                 echo "</div>";
-            endif;
-
+            }
+            wp_reset_postdata();
             ?>
         </ul>
-	</main>
+    </main>
 
 <?php
 get_footer();
