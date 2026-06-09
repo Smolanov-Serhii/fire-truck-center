ed8b924377239ef1d80416c64758366611fe43cb
Serhii <52398335+Smolanov-Serhii@users.noreply.github.com>
2026-06-09 14:06:36 +0300

    init

---
+++ wp-content/themes/fire-truck-center/404.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Fire_Truck_Center
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+        <div class="page-404">
+            <div class="container container-star">
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-1"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+                <div class="star-2"></div>
+            </div>
+            <div class="container container-bird">
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bird bird-anim">
+                    <div class="bird-container">
+                        <div class="wing wing-left">
+                            <div class="wing-left-top"></div>
+                        </div>
+                        <div class="wing wing-right">
+                            <div class="wing-right-top"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-title">
+                    <div class="title">
+                        <div class="number">4</div>
+                        <div class="moon">
+                            <div class="face">
+                                <div class="mouth"></div>
+                                <div class="eyes">
+                                    <div class="eye-left"></div>
+                                    <div class="eye-right"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="number">4</div>
+                    </div>
+<!--                    <div class="subtitle">To main page</div>-->
+                    <a class="button-skew" href="https://firetruck.center/">To main page</a>
+                </div>
+            </div>
+
+        </div>
+
+
+    </main>
+
+<?php
+//get_footer();
